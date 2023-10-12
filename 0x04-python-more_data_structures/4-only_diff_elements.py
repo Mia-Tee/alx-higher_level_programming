fbfefbe5537b377c54ef6a1302a81c4c338f41b2
Mia-Tee <tanyatsekenie@gmail.com>
2023-10-12 12:26:30 -0700

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1, set_2):"""a function that returns a set of all elements present in only one set"""
+def only_diff_elements(set_1, set_2):
     return (set_1 ^ set_2)
