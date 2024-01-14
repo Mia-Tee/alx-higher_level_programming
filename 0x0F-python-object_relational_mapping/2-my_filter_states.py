73d27f06be5f36854c44ab810b9dd61b55d8093b
Mia-Tee <tanyatsekenie@gmail.com>
2024-01-14 12:07:24 -0800

    Filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """
-A script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument
-Connects to a MySQL server running on localhost at port 3306
-Results are sorted in ascending order by states.id
+A Python script that takes in an argument and displays all values in the states
+where `name` matches the argument from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
-    id ASC".format(argv[4]))
-    rows = cursor.fetchall()
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
+
     for row in rows:
-        if row[1] == argv[4]:
-            print(row)
-    cursor.close()
-    db.close()
+        print(row)
